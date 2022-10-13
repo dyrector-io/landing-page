a3ab6e0a35965d19fafcacc37d4303373c3445f8
Bence Nagygyörgy <robot9706@gmail.com>
2022-10-13 08:48:09 +0200

    feat: project setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import styles from "../styles/index.module.css";
+
+const Index: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  );
+};
+
+export default Index;
---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx}'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'dyo-turquoise': '#00d0bf',
+                'dyo-green': '#28c76f',
+                'dyo-purple': '#540ba7',
+                'dyo-purple-light': '#e1ccf8',
+                'dyo-blue': '#00cfe8',
+                'bright': "#d0d2d6",
+                'bright-muted': '#676d7d',
+                'light-eased': '#b4b7bd',
+                'light-grey': '#3b4253',
+                'light-grey-muted': '#404656',
+                'light': '#7783a3',
+                'medium-eased': '#343d55',
+                'medium': '#283046',
+                'dark': '#161d31',
+                'error-red': '#ea5455',
+                'warning-orange': '#ff9f43',
+            },
+            height: {
+                '128': '32rem',
+            },
+            maxHeight: {
+                '128': '32rem',
+            },
+            boxShadow: {
+                'modal': '0 0 20px 5px rgb(0 0 0 / 0.25)',
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [require('@tailwindcss/line-clamp')],
+}
